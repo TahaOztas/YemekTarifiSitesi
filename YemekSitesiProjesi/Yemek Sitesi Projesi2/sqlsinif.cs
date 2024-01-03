dd3456fe9c68d9a63cb2988f2ea9334e0a82777c
TahaOztas <melihoztas54@hotmail.com>
2024-01-03 21:23:41 +0300

    YemekSitesiProjesi
    ASP.NET ile yapılmıştır.

---
+++ YemekSitesiProjesi/Yemek Sitesi Projesi2/sqlsinif.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Yemek_Sitesi_Projesi2
+{
+    public class sqlsinif
+    {
+     //veritabani baglantisi
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection(@"Data Source = CEFUN; Initial Catalog = Dbo_yemektarifi; Integrated Security = true;");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
